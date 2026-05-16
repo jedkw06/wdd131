cc2efd199cdec899e0dd2f619c7471efc6fc012b
jedkw06 <jedkw06@gmail.com>
2026-05-17 00:35:50 +0200

    temples

---
+++ Scripts/temples.js
@@ -0,0 +1,10 @@
+﻿const yearElement = document.getElementById('currentyear');
+const modifiedElement = document.getElementById('lastModified');
+
+if (yearElement) {
+    yearElement.textContent = new Date().getFullYear();
+}
+
+if (modifiedElement) {
+    modifiedElement.textContent = document.lastModified;
+}
